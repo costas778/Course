76071d7f95168fd602978079ac8c62d94421b609
Costas Constantinou <costas778@gmail.com>
2024-09-01 08:22:24 +0300

    Add Heroku domain to ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'coursestaging-9e87cec5aae4.herokuapp.com',
+]
 
 
 # Application definition
